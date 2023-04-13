a2e299410aa7e58fe00db90d0e9ed98fd1c86834
NicKoehler <grillinicola@proton.me>
2023-04-13 09:53:45 +0200

    fix windows

--- src/windows.rs
+++ src/windows.rs
@@ -2,7 +2,7 @@ use crate::download::download_from_url;
 use std::{path::PathBuf, process::Command};
 
 pub fn update(base_url: &str, temp_dir: PathBuf) -> () {
-    let url: String = format!("{}rustdesk-1.2.0-x86_64-pc-windows-msvc.exe", base_url);
+    let url: String = format!("{}rustdesk-1.2.0-x86_64.exe", base_url);
     let temp_path = temp_dir.join("rustdesk.exe").display().to_string();
 
     download_from_url(url, &temp_path);
