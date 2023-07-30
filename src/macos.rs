f36ca7cbcc5d6f57212b12d04c6b9ac56970fc51
NicKoehler <grillinicola@proton.me>
2023-07-30 15:28:24 +0200

    update to new version

--- src/macos.rs
+++ src/macos.rs
@@ -2,14 +2,14 @@ use crate::download::download_from_url;
 use std::{fs::remove_file, path::PathBuf, process::Command};
 
 pub fn update(base_url: &str, temp_dir: PathBuf) -> () {
-    let url = format!("{}rustdesk-1.2.1-x86_64.dmg", base_url);
+    let url = format!("{}rustdesk-1.2.2-x86_64.dmg", base_url);
     let temp_path = temp_dir.join("rustdesk").display().to_string();
 
     download_from_url(url, &temp_path);
     Command::new("sh")
         .arg("-c")
         .arg(format!(
-            "sudo hdiutil attach {};sudo cp -R /Volumes/rustdesk-1.2.1/RustDesk.app /Applications;sudo hdiutil unmount /Volumes/rustdesk-1.2.1",
+            "sudo hdiutil attach {};sudo cp -R /Volumes/rustdesk-1.2.2/RustDesk.app /Applications;sudo hdiutil unmount /Volumes/rustdesk-1.2.2",
             temp_path
         ))
         .output()
--- src/windows.rs
+++ src/windows.rs
@@ -2,7 +2,7 @@ use crate::download::download_from_url;
 use std::{path::PathBuf, process::Command};
 
 pub fn update(base_url: &str, temp_dir: PathBuf) -> () {
-    let url: String = format!("{}rustdesk-1.2.1-x86_64.exe", base_url);
+    let url: String = format!("{}rustdesk-1.2.2-x86_64.exe", base_url);
     let temp_path = temp_dir.join("rustdesk.exe").display().to_string();
 
     download_from_url(url, &temp_path);
