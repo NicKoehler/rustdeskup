4a5cc9fa40664d9de4b0b9290a12750a5b8cf29d
NicKoehler <grillinicola@proton.me>
2023-07-30 15:33:04 +0200

    fix for download status code != 200

--- src/download.rs
+++ src/download.rs
@@ -3,10 +3,13 @@ use std::io::copy;
 
 pub fn download_from_url(url: String, filename: &str) -> () {
     match reqwest::blocking::get(url) {
-        Ok(mut response) => {
-            let mut file = File::create(filename).unwrap();
-            copy(&mut response, &mut file).unwrap();
-        }
+        Ok(mut response) => match response.status() {
+            reqwest::StatusCode::OK => {
+                let mut file = File::create(filename).unwrap();
+                copy(&mut response, &mut file).unwrap();
+            }
+            _ => panic!("Unable to update rustdesk:\n{}", response.status()),
+        },
         Err(e) => panic!("Unable to update rustdesk:\n{}", e),
     }
 }
